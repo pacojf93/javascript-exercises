6c984a595021ece8532300014eb44f06320e4aa1
Paco <127772244+pacojf93@users.noreply.github.com>
2024-12-28 19:21:45 +0100

    exercise 05_sumAll solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(
+        start < 0 || 
+        end < 0 ||
+        !Number.isInteger(start) ||
+        !Number.isInteger(end)
+    ) return "ERROR"
+
+    if(start > end) {
+        const preservedStartValue = start
+        start = end
+        end = preservedStartValue
+    }
+
+    let accumulator = 0
+    for(let i = start; i <= end; i++) {
+        accumulator += i
+    }
+
+    return accumulator
 
 };
 
