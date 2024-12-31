e17f0b569cba770a594d5cfa1e70d6cf894a723c
Paco <127772244+pacojf93@users.noreply.github.com>
2024-12-31 17:17:35 +0100

    exercise 09_palindromes solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    phrase = phrase.split("")
+                .filter(c => c.match(/[a-z]|[0-9]/i))
+                .map(c => c.toLowerCase())
+                .join("")
 
+    const rPhrase = phrase.split("")
+                        .reverse()
+                        .join("")
+    
+    return phrase === rPhrase
 };
 
 // Do not edit below this line
